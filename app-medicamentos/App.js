9b16183f5b67c99cc923149938c98cd9cc5ed058
VMT20 <vnicius.massaranduba@iesb.edu.br>
2025-04-24 09:06:46 -0300

    nome

--- app-medicamentos/App.js
+++ app-medicamentos/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>APP MEDICAMENTO</Text>
+      <text>gay</text>
       <StatusBar style="auto" />
     </View>
   );
